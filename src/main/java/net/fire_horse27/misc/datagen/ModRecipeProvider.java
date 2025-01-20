a23496ddf1b2f67ba57e3572431562d887830358
Fire-Horse27 <firehorse2727@gmail.com>
2025-01-19 22:29:07 -0500

    Updated to 1.21.4

--- src/main/java/net/fire_horse27/misc/datagen/ModRecipeProvider.java
+++ src/main/java/net/fire_horse27/misc/datagen/ModRecipeProvider.java
@@ -2,9 +2,8 @@ package net.fire_horse27.misc.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
-import net.minecraft.data.server.recipe.RecipeExporter;
-import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
-import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
+import net.minecraft.data.recipe.RecipeExporter;
+import net.minecraft.data.recipe.RecipeGenerator;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
@@ -18,53 +17,73 @@ public class ModRecipeProvider extends FabricRecipeProvider {
     }
 
     @Override
-    public void generate(RecipeExporter exporter) {
-        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, Items.CHAIN,4)
-                .pattern("#")
-                .pattern("I")
-                .pattern("#")
-                .input('#', Items.IRON_NUGGET)
-                .input('I', Items.IRON_INGOT)
-                .criterion(hasItem(Items.DROPPER), conditionsFromItem(Items.DROPPER))
-                .offerTo(exporter);
+    protected RecipeGenerator getRecipeGenerator(RegistryWrapper.WrapperLookup registries, RecipeExporter exporter) {
+        return new RecipeGenerator(registries, exporter) {
+            @Override
+            public void generate() {
+                createShaped(RecipeCategory.BUILDING_BLOCKS, Items.CHAIN,4)
+                        .pattern("#")
+                        .pattern("I")
+                        .pattern("#")
+                        .input('#', Items.IRON_NUGGET)
+                        .input('I', Items.IRON_INGOT)
+                        .criterion(hasItem(Items.DROPPER), conditionsFromItem(Items.DROPPER))
+                        .offerTo(exporter);
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.REDSTONE, Items.REDSTONE_TORCH,4)
-                .pattern("#")
-                .pattern("S")
-                .input('#', Items.REDSTONE)
-                .input('S', Items.STICK)
-                .criterion(hasItem(Items.DROPPER), conditionsFromItem(Items.DROPPER))
-                .offerTo(exporter);
+                createShaped(RecipeCategory.REDSTONE, Items.REDSTONE_TORCH,4)
+                        .pattern("#")
+                        .pattern("S")
+                        .input('#', Items.REDSTONE)
+                        .input('S', Items.STICK)
+                        .criterion(hasItem(Items.DROPPER), conditionsFromItem(Items.DROPPER))
+                        .offerTo(exporter);
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, Items.SANDSTONE,2)
-                .pattern("##")
-                .pattern("##")
-                .input('#', Items.SAND)
-                .criterion(hasItem(Items.DROPPER), conditionsFromItem(Items.DROPPER))
-                .offerTo(exporter);
+                createShaped(RecipeCategory.BUILDING_BLOCKS, Items.SANDSTONE,2)
+                        .pattern("##")
+                        .pattern("##")
+                        .input('#', Items.SAND)
+                        .criterion(hasItem(Items.DROPPER), conditionsFromItem(Items.DROPPER))
+                        .offerTo(exporter);
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, Items.RED_SANDSTONE,2)
-                .pattern("##")
-                .pattern("##")
-                .input('#', Items.RED_SANDSTONE)
-                .criterion(hasItem(Items.DROPPER), conditionsFromItem(Items.DROPPER))
-                .offerTo(exporter);
+                createShaped(RecipeCategory.BUILDING_BLOCKS, Items.RED_SANDSTONE,2)
+                        .pattern("##")
+                        .pattern("##")
+                        .input('#', Items.RED_SANDSTONE)
+                        .criterion(hasItem(Items.DROPPER), conditionsFromItem(Items.DROPPER))
+                        .offerTo(exporter);
 
-        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, Items.NETHER_WART_BLOCK)
-                .pattern("##")
-                .pattern("##")
-                .input('#', Items.NETHER_WART)
-                .criterion(hasItem(Items.DROPPER), conditionsFromItem(Items.DROPPER))
-                .offerTo(exporter);
+                createShaped(RecipeCategory.BUILDING_BLOCKS, Items.NETHER_WART_BLOCK)
+                        .pattern("##")
+                        .pattern("##")
+                        .input('#', Items.NETHER_WART)
+                        .criterion(hasItem(Items.DROPPER), conditionsFromItem(Items.DROPPER))
+                        .offerTo(exporter);
 
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, Items.ICE,9)
-                .input(Items.PACKED_ICE)
-                .criterion(hasItem(Items.PACKED_ICE), conditionsFromItem(Items.PACKED_ICE))
-                .offerTo(exporter, "misc:ice");
+                createShapeless(RecipeCategory.BUILDING_BLOCKS, Items.ICE,9)
+                        .input(Items.PACKED_ICE)
+                        .criterion(hasItem(Items.PACKED_ICE), conditionsFromItem(Items.PACKED_ICE))
+                        .offerTo(exporter, "misc:ice");
 
-        ShapelessRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, Items.PACKED_ICE,9)
-                .input(Items.BLUE_ICE)
-                .criterion(hasItem(Items.BLUE_ICE), conditionsFromItem(Items.BLUE_ICE))
-                .offerTo(exporter, "misc:packed_ice");
+                createShapeless(RecipeCategory.BUILDING_BLOCKS, Items.PACKED_ICE,9)
+                        .input(Items.BLUE_ICE)
+                        .criterion(hasItem(Items.BLUE_ICE), conditionsFromItem(Items.BLUE_ICE))
+                        .group("packed_ice")
+                        .offerTo(exporter, "misc:unpacked_ice");
+
+                createShaped(RecipeCategory.BUILDING_BLOCKS, Items.PACKED_ICE)
+                        .pattern("###")
+                        .pattern("###")
+                        .pattern("###")
+                        .input('#', Items.ICE)
+                        .criterion(hasItem(Items.DROPPER), conditionsFromItem(Items.DROPPER))
+                        .group("packed_ice")
+                        .offerTo(exporter);
+            }
+        };
+    }
+
+    @Override
+    public String getName() {
+        return "ModRecipeProvider";
     }
 }
